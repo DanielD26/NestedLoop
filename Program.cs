1d28cb6e8c321fef325ba7f4c02efafb3b5eda79
DANIEL DIAZ <103547416@student.swin.edu.au>
2021-04-01 15:45:27 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NestedLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int counter = 0;
+
+            while (counter < 10) {
+                counter = counter + 1;
+
+                int counter2 = counter;
+
+                while (counter2 < 5) {
+                    Console.WriteLine(name);
+                    counter2 = counter2 + 1;
+                }
+            }
+
+            Console.WriteLine("Goodbye "+ name);
+        }
+    }
+}
